2a61ad735243c63ca5cc026962950de83f53a8eb
Marcus Vaitschulis <21103251+mv-stns@users.noreply.github.com>
2025-01-23 13:02:35 +0100

    Working Directory

---
+++ src/com/pomodoro/App.java
@@ -0,0 +1,36 @@
+package com.pomodoro;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+  @Override
+  public void start(Stage mainStage) throws Exception {
+    Pane root = new Pane();
+    Scene scene = new Scene(root, 1440, 800);
+
+    Text text = new Text("Hello JavaFX");
+    text.setStyle("-fx-font-weight: bold; -fx-font-size: 36px;");
+ 
+    text.setX(50);
+    text.setY(50);
+    root.getChildren().add(text);
+    mainStage.setScene(scene);
+    mainStage.setTitle("Pomodoro App");
+    mainStage.show();
+  }
+
+  public void init() {
+    // Implementation eventueller Business-Logiken
+  }
+  public void stop() {
+    // Freigabe von Resourcen
+  }
+
+  public static void main(String[] args) {
+    Application.launch(App.class, args);
+  }
+ }
